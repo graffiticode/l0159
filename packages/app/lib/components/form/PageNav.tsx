6e0130a81a8cfee7aa8f8c1b45df98ccca2c9dba
Jeff Dyer <jeffdyer@acm.org>
2024-09-26 16:51:08 -0700

    Text tweak

--- packages/app/lib/components/form/PageNav.tsx
+++ packages/app/lib/components/form/PageNav.tsx
@@ -106,7 +106,7 @@ export function PageNav({ state }) {
           </div> ||
           <div className="flex-col">
           <div className="text-xs text-center font-light text-gray-600 pb-2">
-          How well do you know this?
+          How do you feel about this?
           </div>
           <div className="-mt-px flex justify-center items-center px-auto">
           <a
